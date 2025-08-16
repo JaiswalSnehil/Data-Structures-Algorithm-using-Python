2a1134571359232299b01b463c508fb3ab657557
Snehil Jaiswal <112891265+JaiswalSnehil@users.noreply.github.com>
2025-08-16 10:24:56 +0530

    Add files via upload

---
+++ Basic Class and Object Creation.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, model, color):
+        self.model = model
+        self.color = color
+
+    def display_info(self):
+        print(f'Model: {self.model}, color: {self.color}')
+
+car1 = Car("Toyota", "Red")
+car1.display_info()
