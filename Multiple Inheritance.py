e53f83d9a2385ce4efe6e14c1f8332042c4b9a41
Snehil Jaiswal <112891265+JaiswalSnehil@users.noreply.github.com>
2025-08-20 11:36:52 +0530

    Add files via upload

---
+++ Multiple Inheritance.py
@@ -0,0 +1,14 @@
+class Father:
+    def height(self):
+        return "Tall"
+    
+class Mother:
+    def color(self):
+        return "Fair"
+    
+class Child(Father, Mother):
+    pass
+
+child = Child()
+print(child.height())
+print(child.color())
