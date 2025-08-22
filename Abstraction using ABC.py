efb68e77829d77cc06e644bce297726d79bdc783
Snehil Jaiswal <112891265+JaiswalSnehil@users.noreply.github.com>
2025-08-22 10:29:28 +0530

    Add files via upload

---
+++ Abstraction using ABC.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+
+    def area(self):
+        return self.length * self.breadth
+    
+rect = Rectangle(4, 5)
+print("Area:",rect.area())
