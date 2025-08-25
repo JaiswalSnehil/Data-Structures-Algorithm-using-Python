9a7f9f2f3a1377cfa641d32d4258339247942220
Snehil Jaiswal <112891265+JaiswalSnehil@users.noreply.github.com>
2025-08-25 12:05:35 +0530

    Add files via upload

---
+++ Property Decorator (Getter & Setter).py
@@ -0,0 +1,18 @@
+class Product:
+    def __init__(self, price):
+        self._price = price
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value > 0:
+            self._price = value
+        else:
+            print("Invalid Price")
+
+product = Product(100)
+product.price = 200
+print(product.price)
